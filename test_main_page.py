d8090b24c7decb8dbb500adcd88a983baf10d595
Petrov-Vodkin <petrone@bk.ru>
2021-01-18 19:55:19 +0600

    Edit transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -4,30 +4,8 @@ from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
-    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
-    # открываем страницу
     page.open()
-    # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
-    # находим ссылку логина
-    page.should_be_login_link()
-
-def test_login_in_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-
-def test_guest_see_login_forms(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
-
-
-def test_guest_see_register_forms(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
