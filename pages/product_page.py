3da6585f97b4a4ce1e980d6b339faf4cc553e67e
Petrov-Vodkin <petrone@bk.ru>
2021-01-19 17:27:24 +0600

    Calculate and send answer to task from 'alert'

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,4 +4,5 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def should_be_btn_add_to_basket(self):
-        assert self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET), "Button 'Add to basket' is not presented"
+        btn_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        return btn_to_basket
--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+# import time
 from pages.product_page import ProductPage
 
 
@@ -5,4 +6,7 @@ def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_btn_add_to_basket()
+    btn_to_basket = page.should_be_btn_add_to_basket()
+    btn_to_basket.click()
+    page.solve_quiz_and_get_code()
+#    time.sleep(3)
