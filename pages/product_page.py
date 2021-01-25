5da7eab045f2d61c3235f63c96cb65bb4937373c
Petrov-Vodkin <petrone@bk.ru>
2021-01-25 16:47:02 +0600

    Check that no success message after a guest added product to basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,5 +19,5 @@ class ProductPage(BasePage):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
-    def shold_disappear(self):
+    def should_disappear(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "p"
--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,8 @@ from pages.product_page import ProductPage
 num = [i for i in range(10)]
 num[7] = pytest.param("7", marks=pytest.mark.xfail)
 
-@pytest.mark.parametrize("num_promo", [1,2])
+
+@pytest.mark.parametrize("num_promo", [1, num[7]])
 def test_guest_can_add_product_to_basket(browser, num_promo):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_promo}"
     page = ProductPage(browser, link)
@@ -18,6 +19,14 @@ def test_guest_can_add_product_to_basket(browser, num_promo):
     page.check_product_name_in_alert()
     page.check_product_price_in_basket()
     time.sleep(2)
-    page.is_disappeared()
+    page.should_disappear()
 
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    btn_to_basket = page.should_be_btn_add_to_basket()
+    btn_to_basket.click()
+    page.should_not_be_success_message()
 
