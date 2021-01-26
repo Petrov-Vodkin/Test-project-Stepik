4f8a2ca3ee0b7a3514ae49beea2b3b037c3b782b
Petrov-Vodkin <petrone@bk.ru>
2021-01-26 16:07:16 +0600

    Check that guest sees login link on product page

--- test_product_page.py
+++ test_product_page.py
@@ -21,6 +21,13 @@ def test_guest_can_add_product_to_basket(browser, num_promo):
     page.should_disappear()
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
 @pytest.mark.xfail(reason="Fail true (success message should be)")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
