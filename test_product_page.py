775f81683648a39dfbd0b9e33684110b5d8c96b6
Petrov-Vodkin <petrone@bk.ru>
2021-01-28 19:58:55 +0600

    Group tests of product page for login into class

--- test_product_page.py
+++ test_product_page.py
@@ -1,30 +1,24 @@
 import pytest
+
 from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 
-num = [i for i in range(10)]
-num[7] = pytest.param("7", marks=pytest.mark.xfail)
 
+@pytest.mark.login
+class TestLoginFromProductPages:
+    @pytest.fixture(scope="function")
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        self.page = ProductPage(browser, link)
+        self.page.open()
+        yield
+        self.page = None
 
-@pytest.mark.parametrize("num_promo", [num[7]])
-def test_guest_can_add_product_to_basket(browser, num_promo):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_promo}"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_not_be_success_message()
-    btn_to_basket = page.should_be_btn_add_to_basket()
-    btn_to_basket.click()
-    page.solve_quiz_and_get_code()
-    page.check_product_name_in_alert()
-    page.check_product_price_in_basket()
-    page.should_disappear()
+    def test_guest_should_see_login_link_on_product_page(self, setup):
+        self.page.should_be_login_link()
 
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_can_go_to_login_page_from_product_page(self, setup):
+        self.page.go_to_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
@@ -35,13 +29,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_items_to_buy()
     basket_page.should_be_text_basket_is_empty()
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
 
 
 @pytest.mark.xfail(reason="Fail true (success message should be)")
@@ -68,4 +55,22 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     btn_to_basket = page.should_be_btn_add_to_basket()
     btn_to_basket.click()
+    page.should_disappear()
+
+
+num = [i for i in range(10)]
+num[7] = pytest.param("7", marks=pytest.mark.xfail)
+
+
+@pytest.mark.parametrize("num_promo", [num[7]])
+def test_guest_can_add_product_to_basket(browser, num_promo):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_promo}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+    btn_to_basket = page.should_be_btn_add_to_basket()
+    btn_to_basket.click()
+    page.solve_quiz_and_get_code()
+    page.check_product_name_in_alert()
+    page.check_product_price_in_basket()
     page.should_disappear()
