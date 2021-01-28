b6ef5c2a4fc16ebb90aef9703dbd26e8fad586d0
Petrov-Vodkin <petrone@bk.ru>
2021-01-28 13:42:38 +0600

    Group login tests into class

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-# добавляем праметр к тестам "--browser_name" & "--user_language"
+
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
@@ -11,7 +12,6 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
-    # с помощью ф-ии request получаем browser_name, browser_lang заданные в командной строке при запуске теста
     language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     print(f"\nstart {browser_name} browser for test..")
